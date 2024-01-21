da554a3bb59d17d396a04c36d182759e300322b3
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-01-21 13:35:54 +0000

    Update clock.py

--- clock.py
+++ clock.py
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 run = True
 angle = 0
+angle2 = 0
 
 while run:
     
@@ -30,10 +31,20 @@ while run:
         clock_center[0] + hour_hand_length * math.cos(angle),
         clock_center[1] + hour_hand_length * math.sin(angle)
     )
+    
+    
+    angle2+=0.02
+    
+    end2 = (
+        end_point[0] + hour_hand_length * math.cos(angle2),
+        end_point[1] + hour_hand_length * math.sin(angle2),
+    )
 
     # Draw the hour hand
+    
     pygame.draw.line(screen, 'white', clock_center, end_point, 5)
-
+    pygame.draw.line(screen, 'blue', end_point, end2, 3)
+    
     pygame.display.update()
     pygame.time.delay(10)
     
