93e6cbc20741120c97e14763e1607268b0b3f0d6
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-01-21 00:17:12 +0000

    Initial commit

---
+++ draw.py.py
@@ -0,0 +1,41 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 500
+HEIGHT = 500
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+run = True
+
+
+clicked = False
+locations = []
+
+while run:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event)
+            print(event.pos)
+            clicked = True
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            clicked = False
+            
+        if event.type == pygame.QUIT:
+            run = False
+            
+        
+        
+        
+        if clicked:
+            pygame.draw.circle(screen, 'white', event.pos, 2)
+            locations.append(event.pos)
+        
+        pygame.display.update()
+pygame.QUIT
+print(locations)
+
