7d170f8f242c23e081877de603949b5913e91e10
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-01-21 23:35:11 +0000

    source FS

--- draw.py.py
+++ draw.py.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -34,5 +35,43 @@ while run:
         
         pygame.display.update()
 pygame.QUIT 
-print(locations)
+
+
+
+
+def generateEquation(points):
+    n = 2
+    x, y = 0, 0
+    s= 3
+
+    for m in range(len(points) - 1, -1, -1):
+        points[m] = (points[m][0] - 250, 250 - points[m][1])
+        points.append(points[m])
+
+    xt = yt = ""
+    M = len(points)
+    r = []
+    for k in range(-n, n + 1):
+        cx = cy = 0
+        for m in range(M):
+            cx += math.cos(2 * math.pi * k * m / M) * points[m][0] + math.sin(2 * math.pi * k * m / M) * points[m][1]
+            cy += math.cos(2 * math.pi * k * m / M) * points[m][1] - math.sin(2 * math.pi * k * m / M) * points[m][0]
+
+        if k != -n:
+            xt += " + "
+            yt += " + "
+
+        xt += f"{cx / M} cos({k * math.pi}t) - {cy / M} sin({k * math.pi}t)"
+        yt += f"{cx / M} sin({k * math.pi}t) + {cy / M} cos({k * math.pi}t)"
+        r.append((cx / M, cy / M))
+
+    eq = f"({x} + {s}({xt}), {y} + {s}({yt}))".replace(" + -", " - ").replace(" - -", " + ")
+    return eq, r
+
+eq, r = (generateEquation(locations))
+
+print(f"\n\n\n{r}\n\n{eq}")
+
+
+
 
